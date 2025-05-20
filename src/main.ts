cb36cfef0b295f822743a6f8c34e8135b2ce50a7
Nadin <you@example.com>
2025-05-20 11:49:12 +0300

    feat: Pino added

---
+++ src/main.ts
@@ -0,0 +1,67 @@
+import express from 'express';
+
+const server = express();
+server.use(express.json()); // Включаем парсер тела
+
+const port = 2000;
+
+
+server.get('/', (req, res) => {
+      res.json({key: 'value'})
+})
+
+server.get('/task', (req, res) => {
+    res.send('Список задач');
+});
+
+server.get('/task/:id', (req, res) => {
+    res.send('Одна задача');
+});
+
+server.get('/task/my/authored', (req, res) => {
+    res.send('Список своих созданных задач');
+});
+
+server.get('/task/my/assigned', (req, res) => {
+    res.send('Список своих назначенных задач');
+});
+
+server.get('/user/:id', (req, res) => {
+    const { id } = req.params;
+    res.send(`Пользователь ${id}, держите новости!`);
+});
+
+server.get('/user/:id/news/:newsId', (req, res) => {
+    const id: string = req.params.id;
+    const newsId: string = req.params.newsId;
+
+    res.send(`Держите новости ${ id }  ${newsId}!`);
+});
+
+server.post('', (req, res) => {
+    console.log('Пришло тело:');
+    console.log(req.body);
+    res.send('Hello World!');
+});
+
+server.post('/payload', (req, res) => {
+    console.log(req.body);
+    // res.send(req.query);
+});
+
+server.post('/task', (req, res) => {
+    console.log('Пришел запрос с методом POST');
+    res.send('Создать задачу');
+});
+
+server.put('/task/:id', (req, res) => {
+    res.send('Обновить задачу');
+});
+
+server.delete('/task/:id', (req, res) => {
+    res.send('Удалить задачу');
+});
+
+server.listen(port, () => {
+    console.log(`Server is started on port ${port}...`);
+});
