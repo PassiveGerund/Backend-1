3a056e2db176e14b9ae36e13386bc61eb6d52b2b
Nadin <you@example.com>
2025-05-27 12:53:13 +0300

    fix: installed express@4

--- src/main.ts
+++ src/main.ts
@@ -12,18 +12,18 @@ server.use(express.json()); // Включаем парсер тела
 
 const port = 2000;
 
-server.get('/', (req, res) => {
-  logger.info('Главная страница');
-  res.json({ key: 'value123' });
-});
-
-server.post('', (req, res) => {
-  // console.log('Пришло тело:');
-  // console.log(req.body);
-  const body = JSON.stringify(req.body);
-  logger.info(`Пришло тело: ${body}  !`);
-  res.send(`Пришло тело: ${body}`);
-});
+// server.get('/', (req, res) => {
+//   logger.info('Главная страница');
+//   res.json({ key: 'value123' });
+// });
+//
+// server.post('', (req, res) => {
+//   // console.log('Пришло тело:');
+//   // console.log(req.body);
+//   const body = JSON.stringify(req.body);
+//   logger.info(`Пришло тело: ${body}  !`);
+//   res.send(`Пришло тело: ${body}`);
+// });
 
 logRoutes(server);
 
