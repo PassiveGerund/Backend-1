63e7f2bb50f1d654941a16bc64833f020d903f41
Nadin <you@example.com>
2025-06-04 13:18:43 +0300

    feat: added DTO

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,16 +1,22 @@
-import { NextFunction, Request, Response } from 'express';
+import 'reflect-metadata';
+import { IsEmail } from 'class-validator';
 
-const middleware1 = (req: Request, res: Response, next: NextFunction) => {
-  console.log(`Пришел запрос с методом ${req.method} на путь ${req.url}`);
-  next();
-};
-const middleware2 = (req: Request, res: Response, next: NextFunction) => {
-  console.log(`Выполнился middleware2`);
-  next();
-};
-const middleware3 = (req: Request, res: Response, next: NextFunction) => {
-  console.log(`Выполнился middleware3`);
-  next();
-};
-
-export { middleware1, middleware2, middleware3 };
+export class CheckEmailDTO {
+  @IsEmail()
+  email: string;
+}
+//
+// const middleware1 = (req: Request, res: Response, next: NextFunction) => {
+//   console.log(`Пришел запрос с методом ${req.method} на путь ${req.url}`);
+//   next();
+// };
+// const middleware2 = (req: Request, res: Response, next: NextFunction) => {
+//   console.log(`Выполнился middleware2`);
+//   next();
+// };
+// const middleware3 = (req: Request, res: Response, next: NextFunction) => {
+//   console.log(`Выполнился middleware3`);
+//   next();
+// };
+//
+// export { middleware1, middleware2, middleware3 };
